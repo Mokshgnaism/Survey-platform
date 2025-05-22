5344a0b29f4bf3c6111f05b8be8a902932735ad2
Mokshgnaism <159766150+Mokshgnaism@users.noreply.github.com>
2025-05-22 08:37:23 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
 app.use(cookieParser());
-import cors from "cors";
+
 
 const allowedOrigins = [
   "http://localhost:5173", 
