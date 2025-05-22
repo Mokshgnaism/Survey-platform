d9693aada6a920c7d03b8ec8d4c1da96b818ba5e
Mokshgnaism <159766150+Mokshgnaism@users.noreply.github.com>
2025-05-22 09:15:48 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-const BASE_URL = https://survey-platform-1.onrender.com/api;
+
+const BASE_URL = "https://survey-platform-1.onrender.com/api";
+
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, 
