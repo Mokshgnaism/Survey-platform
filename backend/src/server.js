e382db22744cebec2fee839671647766e394ab4a
Gorantla Mokshgna <gorantlamokshgna980@gmail.com>
2025-05-22 07:43:29 +0530

    Initial commit with backend and frontend

---
+++ backend/src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js";
+import cors from 'cors';
+dotenv.config();
+const app = express();
+import dbConnect from "./lib/dbConnect.js";
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // if you ever post form data
+
+app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:5173',  // allow your front-end
+  credentials: true,                // enable Set-Cookie and Cookie headers
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization','X-Requested-With']
+}));
+app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
+dbConnect();
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
